ce32a341324d49f7c2cdf5efcbebaa474e543809
John Salas <salas.john@hotmail.com>
2020-10-04 22:47:51 -0500

    index2

--- js/scripts.js
+++ js/scripts.js
@@ -14,5 +14,41 @@ $(document).ready(function() {
         event.preventDefault();
         $('#navbar').collapse('hide');
     });
+
+    $('.slick_demo_2').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        centerMode: true,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        adaptiveHeight: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
     
 });
